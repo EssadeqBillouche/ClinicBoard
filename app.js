83a8df8ab06de63114806058a0043e4d76e0dfe0
MrNobodyCode <essadeq.billouche@gmail.com>
2025-09-17 10:43:39 +0200

    create the app.js with routing system

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-addEventListener(document)
+import {renderHome} from './components/home'
+import {dashboard} from './components/dashboard'
+import {login} from './components/login'
+
+
+const routes = {
+    '/home' : renderHome,
+    '/login' : login,
+    '/dashboard' : dashboard
+}
+
+
+function router(){
+    const path  = location.hash.split(1);
+    const page = routes[path];
+
+    if(page != null){
+        page();
+    }
+    else {
+        home();
+    }
+
+}
+
+window.addEventListener('load', router);
+window.addEventListener('hachChange', router);
