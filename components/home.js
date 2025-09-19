7a2b087d777b419cb4d5d9ed0b82a4e908de2e83
MrNobodyCode <essadeq.billouche@gmail.com>
2025-09-19 12:12:06 +0100

    home coponenet

--- components/home.js
+++ components/home.js
@@ -1,3 +1,12 @@
-export default function home(){
-    render()
+export default function renderHome(){
+    const RootElement = document.getElementById('root');
+    
+    RootElement.innerHTML = `
+    <section class="page">
+      <h1>Bienvenue 👋</h1>
+      <p>Gérez vos patients, rendez-vous et finances en toute simplicité.</p>
+      <button onclick="location.hash='#/dashboard'">Aller au tableau de bord</button>
+    </section>
+  `;
+    
 }
