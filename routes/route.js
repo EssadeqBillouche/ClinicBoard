e61be431956e21d6a7834250d7207a7c9d02c587
MrNobodyCode <essadeq.billouche@gmail.com>
2025-09-19 09:14:15 +0100

    delete no needed files

--- routes/route.js
+++
@@ -1,8 +0,0 @@
-import {login} from '../components/login.js'
-import {home} from '../components/home.js'
-import { renderSingUp } from '../components/Auth.js'
-
-
-const page = {
-    "singUp" : {renderSingUp}
-}
