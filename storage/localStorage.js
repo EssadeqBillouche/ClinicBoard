bb359705cf879f323096e2fdef00ef2034fe374c
MrNobodyCode <essadeq.billouche@gmail.com>
2025-09-19 16:10:35 +0100

    add user and verify password logic

--- storage/localStorage.js
+++ storage/localStorage.js
@@ -33,7 +33,40 @@ function saveToStorage(data){
     localStorage.setItem(storageKey ,JSON.stringify(data));
 }
 
+export async function addAdmin(data){
+    const data = getAllStorage;
+    const hashedPass = await hashPass(data);
 
+    data.user.password = hashedPass;
+    data.user.attamptTolog = 0;
+    data.user.locked = null;
+    saveToStorage(data);
+}
 
+export async function verifyPassword(password){
+    
+    
+    const currentTime = Date.now();
+    const data = getAllStorage();
 
+    const isItValidPass = (await hashPass(password)) === data.user.password ;
 
+    if(!isItValidPass)
+        {
+        data.user.attamptTolog += 1
+         if(data.user.attamptTolog >= 3){
+            data.user.locked = currentTime + 90000
+            data.user.attamptTolog = 0
+         }
+         saveToStorage(data);
+         return {success : false, message : "invalid password" }
+    }
+
+    if (data.user.locked < currentTime){
+        return { success : false, message : "your account has been blockd"}
+    }
+
+    if(isItValidPass){
+        return true
+    }
+}
